092e80a4bf26c02377f5792b28378adf30522cfa
EYE <EYE@EYEz>
2023-07-05 22:18:29 +0300

    Добавит компонент страницы предложения

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,11 @@
-import MainPage from './main';
+import LoginPage from './login-page';
+import MainPage from './main-page';
+import OfferPage from './offer-page';
 
 type AppProps = {
   proposals: number;
 }
 
 export default function App({proposals}: AppProps) {
-  return <MainPage proposals={proposals}></MainPage>;
+  return <OfferPage proposals={proposals}></OfferPage>;
 }
