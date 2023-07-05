bc242eaa81f4ffcaf229479275f209bc6b7fa705
EYE <EYE@EYEz>
2023-07-05 22:23:42 +0300

    Отобразит главную страницу

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,9 @@
-import LoginPage from './login-page';
 import MainPage from './main-page';
-import OfferPage from './offer-page';
 
 type AppProps = {
   proposals: number;
-}
+};
 
-export default function App({proposals}: AppProps) {
-  return <OfferPage proposals={proposals}></OfferPage>;
+export default function App({ proposals }: AppProps) {
+  return <MainPage proposals={proposals}></MainPage>;
 }
