5fe999968bd69120e71b90ae450a42eb90dad41c
EYE <EYE@EYEz>
2023-08-28 18:36:05 +0300

    инициализирует хранилище

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Settings } from './const';
 import { offers } from './mocks/offer';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App proposalsCount={Settings.ProposalsCount} offers={offers}/>
+    <Provider store={store}>
+      <App proposalsCount={Settings.ProposalsCount} offers={offers} />
+    </Provider>
   </React.StrictMode>
 );
